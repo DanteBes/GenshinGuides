e7591fcf12fcc39cb651f981f26f257c0258f31b
DanteBes <besfalinfreelancer@gmail.com>
2024-08-23 13:35:05 +0300

    Updated header and footer: Added working links to header, replaced "Разработчик" with "Мой вк", removed "Доп.ссылки" and dropdown menu styles, deleted JS script, and updated footer link.

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const btnLink = document.querySelector(".button-menu");
-
-const lock = document.querySelector(".hidden");
-
-btnLink.addEventListener('click', () => {
-  lock.classList.toggle("hidden")
-})
