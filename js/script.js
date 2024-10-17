1f8f28d036ca4f0a465ffb6cbb404ba66f301606
DanteBes <besfalinfreelancer@gmail.com>
2024-10-17 13:35:41 +0300

    Fixed grammatical error

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 const btnSvg = document.getElementById('info_svg-btn');
 
-btnSvg.addEventlistener('click', () => {
+btnSvg.addEventListener('click', () => {
   console.log("svg?");
 });
-
-//const btnSvg = document.getElementById('info_svg-btn');
-  //btnSvg.addEventListener('click', () => {
-    //console.log('svg? ');
-//});
