0fdd5bc6f9caf5ed31b4d98bdde2affe19dac5af
DanteBes <besfalinfreelancer@gmail.com>
2024-10-17 13:16:14 +0300

    Created event handler

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const btnSvg = document.getElementById('info_svg-btn');
+
+btnSvg.addEventlistener('click', () => {
+  console.log("svg?");
+});
+
+//const btnSvg = document.getElementById('info_svg-btn');
+  //btnSvg.addEventListener('click', () => {
+    //console.log('svg? ');
+//});
