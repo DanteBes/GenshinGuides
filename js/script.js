fb60f58c9fd4fd04b31c48b0655a899574ceb410
DanteBes <besfalinfreelancer@gmail.com>
2024-10-21 13:25:17 +0300

    In HTML, updated text information for the "p" tag with the class "info__block-none-text". Removed height, added text-align, and rewrote top and right for the class "info__block_none". Set display: none for the id "info__block-none" and updated the event handler for the svg.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const btnSvg = document.getElementById('info_svg-btn');
 
+const info__block_none = document.getElementById('info__block-none');
+
 btnSvg.addEventListener('click', () => {
-  console.log("svg?");
+  btnSvg.classList.toggle('info__block-none');
 });
