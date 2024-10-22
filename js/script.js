aa1d8ffeb6671399f8a943387f33d83992ed6920
DanteBes <besfalinfreelancer@gmail.com>
2024-10-22 13:28:10 +0300

    Commented out the old code that toggled the 'info__block-none' class and replaced it with a simple alert function that shows a message when the button is clicked.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
+//const btnSvg = document.getElementById('info_svg-btn');
+
+//const info__block_none = document.getElementById('info__block-none');
+
+//btnSvg.addEventListener('click', () => {
+  //btnSvg.classList.toggle('info__block-none');
+//});
+
 const btnSvg = document.getElementById('info_svg-btn');
 
-const info__block_none = document.getElementById('info__block-none');
-
 btnSvg.addEventListener('click', () => {
-  btnSvg.classList.toggle('info__block-none');
+  alert("Тут, возможно, будет мини-календарик, но жаль, что автор сайта не знает джаваскрипт😥");
 });
